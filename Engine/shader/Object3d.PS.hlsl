eb0ec396b893093e6c9e30c07fc80f322f524aec
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-07-12 12:57:18 +0900

    BlinnPhongReflectionModelを実装

--- Engine/shader/Object3d.PS.hlsl
+++ Engine/shader/Object3d.PS.hlsl
@@ -55,9 +55,18 @@ PixelShaderOutput main(VertexShaderOutput input)
         */
         
         float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
+        
+        //フォン反射モデル
+        /*
         float32_t3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
         float RdotE = dot(reflectLight, toEye);
         float specularPow = pow(saturate(RdotE), gMaterial.shininess);  //反射強度
+        */
+        
+        //ブリン・フォン反射モデル
+        float32_t3 halfVector = normalize(-gDirectionalLight.direction + toEye);
+        float NDotH = dot(normalize(input.normal), halfVector);
+        float specularPow = pow(saturate(NDotH), gMaterial.shininess);
         
         //拡散反射
         float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
